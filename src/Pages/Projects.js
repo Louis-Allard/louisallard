8e4ce072067d4afd37fee4f3b28d41729f500b8e
Louis Allard <la.devweb@yahoo.com>
2021-09-17 17:35:15 +0200

    Place first els.

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -7,6 +7,16 @@ const Projects = () => {
         <div className="projects">
             <Menu />
             <Title text="Mes projets" />
+            <div className="els">
+                    <div className="el">1</div>
+                    <div className="el">2</div>
+                    <div className="el">3</div>
+                    <div className="el">4</div>
+                    <div className="el">5</div>
+                    <div className="el">6</div>
+                    <div className="el">7</div>
+                    <div className="el">8</div>
+                </div>
         </div>
     );
 };
