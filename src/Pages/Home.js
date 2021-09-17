0fa322b8480b613ebdd3e686305f93554c09b144
Louis Allard <la.devweb@yahoo.com>
2021-09-17 11:41:40 +0200

    Add texte effect.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Menu from '../Components/Menu';
+import { landing } from "./lng/fr";
 
 const home = () => {
     return (
         <div className="home">
            <Menu />
            <div className="data-container">
-           <div className="txt">
-               Landing page
+        <div className="txt">{ landing.name }</div>
+           <div className="title">
+           <div>Developpeur </div> 
+            <div>
+                <span>de solutions</span>
+            </div>
            </div>
            </div>
         </div>
     );
 };
-
 export default home;
