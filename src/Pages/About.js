3de87995db36c32c0b1f5a16221370a7d71a78a0
Louis Allard <la.devweb@yahoo.com>
2021-09-17 17:17:04 +0200

    Add and move assets.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,9 +3,24 @@ import DOMPurify from 'dompurify';
 import Menu from '../Components/Menu';
 import Title from '../Components/Title';
 import { aboutme } from '../lng/fr.js';
-import conseils from '../assets/conseils.png';
-import expertise from '../assets/expertise.png';
-import confiance from '../assets/confiance.png';
+import conseils from '../assets/about/conseils.png';
+import expertise from '../assets/about/expertise.png';
+import confiance from '../assets/about/confiance.png';
+import html from '../assets/about/html.png';
+import bootstrap from '../assets/about/bootstrap.png';
+import css from '../assets/about/css.png';
+import git from '../assets/about/git.png';
+import github from '../assets/about/github.png';
+import js from '../assets/about/js.png';
+import linux from '../assets/about/linux.png';
+import mysql from '../assets/about/mysql.png';
+import node from '../assets/about/node.png';
+import php from '../assets/about/php.png';
+import python from '../assets/about/python.png';
+import redux from '../assets/about/redux.png';
+import vuejs from '../assets/about/vuejs.png';
+import react from '../assets/about/react.png';
+
 
 const About = () => {
 
@@ -26,6 +41,25 @@ const About = () => {
                 <div className="card">
                 <img src={ confiance } alt="confiance" />
                 <p dangerouslySetInnerHTML={{__html: DOMPurify.sanitize( aboutme.confiance)}} />
+                </div>
+            </div>
+            <div className="skills">
+                <div className="myskills" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize( aboutme.myskills)}} />
+                <div className="skillsico">
+                    <div className="el"><img src={html} alt="html" title="html" /></div>
+                    <div className="el"><img src={css} alt="css" title="css"/></div>
+                    <div className="el"><img src={bootstrap} alt="bootstrap" title="bootstrap" /></div>
+                    <div className="el"><img src={linux} alt="linux" title="linux" /></div>
+                    <div className="el"><img src={git} alt="git" title="git" /></div>
+                    <div className="el"><img src={github} alt="github" title="github" /></div>
+                    <div className="el"><img src={js} alt="js" title="js" /></div>
+                    <div className="el"><img src={node} alt="node" title="node" /></div>
+                    <div className="el"><img src={mysql} alt="mysql" title="mysql" /></div>
+                    <div className="el"><img src={php} alt="php" title="php" /></div>
+                    <div className="el"><img src={python} alt="python" title="python" /></div>
+                    <div className="el"><img src={redux} alt="redux" title="redux" /></div>
+                    <div className="el"><img src={vuejs} alt="vuejs" title="vuejs" /></div>
+                    <div className="el"><img src={react} alt="react" title="reactjs" /></div>
                 </div>
             </div>
         </div>
