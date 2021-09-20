12bdc0869c5cd709bc4c27e4c2bb1d67813286ce
Louis Allard <la.devweb@yahoo.com>
2021-09-20 11:10:28 +0200

    Stylish project page.

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import DOMPurify from 'dompurify';
 
 const Project = (props) => {
     return (
         <div className="project">
-            <img src={ props.src } alt={ props.alt } title={ props.title } desc={ props.desc } />
+            <img src={ props.src } alt={ props.alt } title={ props.title } /><br />
+            <span className="name">{ props.name }</span>
+            <div className="desc" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.desc) }} />
         </div>
     );
 };
