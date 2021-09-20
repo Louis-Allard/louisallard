85c062c9ede348605e0e7e16dc02efd919e55ce3
Louis Allard <la.devweb@yahoo.com>
2021-09-20 16:09:55 +0200

    Add cv files.

---
+++ src/Pages/Cv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../Components/Title';
+
+const cv = () => {
+    return (
+        <div>
+            <Title text="Mon Parcours" />
+        </div>
+    );
+};
+
+export default cv;
