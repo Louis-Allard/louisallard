013d0a00d7c2fae9d06a7f36ad5bd2134d7f36b5
Louis Allard <la.devweb@yahoo.com>
2021-09-21 15:59:56 +0200

    Do Footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt, faPhone, faCodeBranch, faUsers } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
         <div className="footer">
-            Footer
+           <div className="els">
+               <p><FontAwesomeIcon icon={faAt} />: contact@louisallard.fr</p>
+               <p><FontAwesomeIcon icon={faPhone} />: 07 67 82 69 67</p>
+               <p><FontAwesomeIcon icon={faUsers} />: <a href="https://www.linkedin.com/in/louis-allard-devweb/" target="_blank">Mon Linkedin</a></p>
+               <p><FontAwesomeIcon icon={faCodeBranch} />: <a href="https://github.com/Louis-Allard" target="_blank">Mon github</a></p>
+           </div>
         </div>
     );
 };
