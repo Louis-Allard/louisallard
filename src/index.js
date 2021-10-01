d33c9b089ed1be365654e08a279b42655dd33f99
Louis Allard <la.devweb@yahoo.com>
2021-10-01 16:09:57 +0200

    Add react-ga package.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './styles/index.scss';
 import App from './App';
+
+ReactGA.initialize('G-8TQ1FSX5QZ');
 
 ReactDOM.render(
   <React.StrictMode>
