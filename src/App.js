240523c3be12fffe526e7e20643fcb068b79c1f1
Louis Allard <la.devweb@yahoo.com>
2021-10-04 16:37:47 +0200

    Add Hashrouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
@@ -11,7 +11,7 @@ import NotFound from './Pages/NotFound';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        <HashRouter>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
@@ -21,7 +21,7 @@ function App() {
           <Route path="/apropos" exact component={Apropos} />
           <Route component={NotFound} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
