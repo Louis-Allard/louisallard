acb43befae26ffe379bc81623a8a4f06ab9bc9cd
Louis Allard <50803654+Louis-Allard@users.noreply.github.com>
2022-08-01 18:09:25 +0200

    Add nodemailer secure.

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const app = express();
 const { portNumber} = require("./conf");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const xoauth2 = require('xoauth2');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("./tmp"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+require("dotenv").config();
 app.listen(5000, () => console.log(":. Server Running .:"));
 
 // Test
@@ -17,14 +19,22 @@ app.get('/', function (req, res) {
   res.send(':. Node test is ok .:');
 });
 
-
 // Nodemailer
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  type: 'OAuth2',
+  service: 'protonmail',
+  host: process.env.MAIL_HOST,
+  port: 465,
+  secure: true, // true for 465, false the other ports
   auth: {
-    user: "@gmail.com",
-    pass: "",
+    xoauth2: xoauth2.createXOAuth2Generator({
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PWD,
+    })
   },
+  tls: {
+    rejectUnauthorized: false
+}
 });
 
 contactEmail.verify((error) => {
